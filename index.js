ec5db6ad99c5ddcee59551ffee9581232db2938d
Stanislav <verbatim8872@gmail.com>
2021-10-12 13:30:42 +0300

    add user and token models

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const start = async () => {
+    try{
+        await mongoose.connect(`mongodb+srv://admin:admin@cluster0.c4817.mongodb.net/jwt-auth?retryWrites=true&w=majority`)
+        app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start()
